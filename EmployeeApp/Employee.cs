c6262ca179601173000a46d8d887af746ca2c203
lagbullet <40629577+lagbullet@users.noreply.github.com>
2018-07-01 12:23:55 +0300

    6

---
+++ EmployeeApp/Employee.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class PInfoCompare : IComparer<Employee>
+{
+    public int Compare(Employee empl1, Employee empl2)
+    {
+        if (empl1.SurName == empl2.SurName)
+            if (empl1.FirstName == empl2.FirstName)
+                if (empl1.Patronymic == empl2.Patronymic)
+                {
+                    return 0;
+                }
+                else
+                {
+                    if (empl1.Patronymic.CompareTo(empl2.Patronymic) == -1)
+                        return -1;
+                    else
+                        return 1;
+                }
+            else
+            {
+                if (empl1.FirstName.CompareTo(empl2.FirstName) == -1)
+                    return -1;
+                else
+                    return 1;
+            }
+        else
+        {
+            if (empl1.SurName.CompareTo(empl2.SurName) == -1)
+                return -1;
+            else
+                return 1;
+        }
+    }
+}
+
+public class Employee : IComparable<Employee>
+{
+    public String surName;
+    public String firstName;
+    public String patronymic;
+    public String birthdate;
+    public float salary;
+
+    public String SurName { get; set; }
+    public String FirstName { get; set; }
+    public String Patronymic { get; set; }
+    public String Birthdate { get; set; }
+    public float Salary { get; set; }
+
+    public Employee(String Sname, String Fname, String patronymic, String Bdate, float salary)
+	{
+        SurName = Sname;
+        FirstName = Fname;
+        Patronymic = patronymic;
+        Birthdate = Bdate;
+        Salary = salary;
+	}
+
+    public int CompareTo(Employee other)
+    { 
+        {
+            if (Salary == other.Salary)
+                return 0;
+            else if (Salary < other.Salary)
+                return -1;
+            else
+                return 1;
+        }
+    }
+
+    override public string ToString()
+    {
+        return string.Format("Surname: " + SurName + "\nFirstname: " + FirstName + "\nPatronymic:" + Patronymic + "\nBirthdate: " + Birthdate + "\nSalary: " + Salary);
+    }
+}
---
+++ EmployeeApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmployeeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> Employees = new List<Employee>
+            {
+                new Employee("Surname3", "FirstName3", "Patronymic3", "2000-01-01", 500),
+                new Employee("Surname1", "FirstName1", "Patronymic1", "2001-01-01", 350),
+                new Employee("Surname2", "FirstName2", "Patronymic2", "2002-01-01", 550),
+                new Employee("Surname0", "FirstName0", "Patronymic0", "1995-01-01", 400)
+            };
+            Employees.Sort(new PInfoCompare());
+            foreach (Employee empl in Employees)
+            {
+                Console.WriteLine("Employee");
+                Console.WriteLine(empl.ToString());
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+            Employees.Sort();
+            foreach (Employee empl in Employees)
+            {
+                Console.WriteLine("Employee");
+                Console.WriteLine(empl.ToString());
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
